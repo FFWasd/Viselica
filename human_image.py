b743706a16cd9b9bdd39017591257b586cf8e418
VERAL0M <akveduk2018@gmail.com>
2025-11-09 23:38:14 +0500

    Создал новое меню и добавил проверку на ввод русских букв

---
+++ human_image.py
@@ -0,0 +1,241 @@
+
+
+litle_man="""
+░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((    ░░░░
+░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((   ░░
+  ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓▓llB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+  ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█((((░██░      
+ (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓▓ooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░ ▓      
+ (░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB████▒▒(    
+((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓▓ol▓▓ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓░   █(((  
+    ((((BBBBllllllloBooooooolooooooollolllll BBBB((((  ▓▓█▓
+      ((  ((BBBB(▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓ BBBB((((
+     (((B  BBBlllllloool▒        ░        ▒(oooolllllBBBB(((
+ (   (  ((BB  BBllllooool░ occcl▓▓▓lcccc ░ oooolllBBBB(((((
+ ((   BB  BBBB  llloooooo ░ occl░(░(cco ░oooolllllBBB(((( 
+ ((B B  BB  Blll  looooooo ░░B(l▓▒░l(B(░░oooollllllBBBB((((
+( (BB BB  ll  lloo  ooooocco ▓ lB▓Bl ▓░occcollllllllBBBB(((
+(( BBB BBl  ll  looo  oooccccc▒▓▓█▓▓▓░ccccoollllllllBBBBB((
+(( BBBB Blll  lo  oooo  occccc(░B(B░░ccccoollllllllBBBBBB((
+((B BBBB llllo  oo  oooo  ccccc▒ B(▒lcccoolllllllllBBBBB(((
+(((B BBBB lllooo  oo  oooc  ccl▓▓▒▓▓(ccoollllllllllBBBBB(((
+((((B BBBB llloooo  oo  oooo   ▒▓█▓▒░oooollllllllllBBBB((((
+(((((B BBBl lllooooo  oooo (   ▒▒(░▒olllllllllllBBBB(((((
+((((((B BBBl l  lloooo  oooo   ▒░(░▒oolllllllllBBBB((((((
+ (((((BB  BB  ll llllllo  o ooB░░(░▒(olllllllllllBBB(((((( 
+  (((((BB   BB  ll lllllll  lll▒▒B▒▓lllllllllllBBBBB(((((  
+   (((((BB   B  BB  B Blllllll ░▓(▓░ llllllllBBBBB((((((   
+    ((((((B   BB  BB   BBBllll(▒▒(▒░ llllBBBBBBB(((((((    
+     ((((((( (  BB  B   BBBBBBB▒▒B░▒BBBBBBBBB((((((((             
+░░       (((( ((  ((   (  BBBBB░▒ ▒░BBBBBB((((((((                ░░
+░░░░        (( (((  (   ((  (((▓▓(▒▒(((((((((((                 ░░░░
+░░░░░░            (( (  ((((((((((((                          ░░░░░░
+░░░░░░░░░░                (                               ░░░░░░░░░░ 
+"""
+
+viselica={
+    0: """
+░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((    ░░░░
+░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((   ░░
+  ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓▓llB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+  ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█((((░██░      
+ (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓▓ooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░ ▓      
+ (░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB████▒▒(    
+((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓▓ol▓▓ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓░   █(((  
+    ((((BBBBllllllloBooooooolooooooollolllll BBBB((((  ▓▓█▓
+      ((  ((BBBB(▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓ BBBB((((
+     (((B  BBBlllllloool         ░         (oooolllllBBBB(((""".replace('B', " ").replace("(", " ").replace('l', " ").replace('o', ' ').replace('c', " "),
+      1: """
+░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((    ░░░░
+░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((   ░░
+  ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓▓llB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+  ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█((((░██░      
+ (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓▓ooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░ ▓      
+ (░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB████▒▒(    
+((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓▓ol▓▓ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓░   █(((  
+    ((((BBBBllllllloBooooooolooooooollolllll BBBB((((  ▓▓█▓
+      ((  ((BBBB(▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓ BBBB((((
+     (((B  BBBlllllloool         ░         (oooolllllBBBB(((
+ (   (  ((BB  BBllllooool  occcl▓▓▓lcccc   oooolllBBBB(((((
+ ((   BB  BBBB  llloooooo   occl░(░(cco  oooolllllBBB(((( 
+ ((B B  BB  Blll  looooooo   B(l▓▒░l(B(  oooollllllBBBB((((""".replace('B', " ").replace("(", " ").replace('l', " ").replace('o', ' ').replace('c', " "),
+
+
+  2:"""
+░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((    ░░░░
+░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((   ░░
+  ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓▓llB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+  ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█((((░██░      
+ (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓▓ooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░ ▓      
+ (░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB████▒▒(    
+((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓▓ol▓▓ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓░   █(((  
+    ((((BBBBllllllloBooooooolooooooollolllll BBBB((((  ▓▓█▓
+      ((  ((BBBB(▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓ BBBB((((
+     (((B  BBBlllllloool         ░         (oooolllllBBBB(((
+ (   (  ((BB  BBllllooool  occcl▓▓▓lcccc   oooolllBBBB(((((
+ ((   BB  BBBB  llloooooo   occl░(░(cco   oooolllllBBB(((( 
+ ((B B  BB  Blll  looooooo   B(l▓▒░l(B(  oooollllllBBBB((((
+( (BB BB  ll  lloo  ooooocco   lB▓Bl   occcollllllllBBBB(((
+(( BBB BBl  ll  looo  oooccccc▒▓▓█▓▓▓░ccccoollllllllBBBBB((
+(( BBBB Blll  lo  oooo  occccc(░B(B░░ccccoollllllllBBBBBB((
+((B BBBB llllo  oo  oooo  ccccc▒ B(▒lcccoolllllllllBBBBB(((
+(((B BBBB lllooo  oo  oooc  ccl▓▓▒▓▓(ccoollllllllllBBBBB(((
+((((B BBBB llloooo  oo  oooo   ▒▓█▓▒░oooollllllllllBBBB((((
+ """.replace('B', " ").replace("(", " ").replace('l', " ").replace('o', ' ').replace('c', " "),
+ 3: """
+░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((    ░░░░
+░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((   ░░
+  ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓▓llB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+  ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█((((░██░      
+ (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓▓ooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░ ▓      
+ (░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB████▒▒(    
+((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓▓ol▓▓ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓░   █(((  
+    ((((BBBBllllllloBooooooolooooooollolllll BBBB((((  ▓▓█▓
+      ((  ((BBBB(▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓ BBBB((((
+     (((B  BBBlllllloool▒        ░         (oooolllllBBBB(((
+ (   (  ((BB  BBllllooool░ occcl▓▓▓lcccc   oooolllBBBB(((((
+ ((   BB  BBBB  llloooooo ░ occl░(░(cco  oooolllllBBB(((( 
+ ((B B  BB  Blll  looooooo ░░B(l▓▒░l(B(  oooollllllBBBB((((
+( (BB BB  ll  lloo  ooooocco ▓ lB▓Bl   occcollllllllBBBB(((
+(( BBB BBl  ll  looo  oooccccc▒▓▓█▓▓▓░ccccoollllllllBBBBB((
+(( BBBB Blll  lo  oooo  occccc(░B(B░░ccccoollllllllBBBBBB((
+((B BBBB llllo  oo  oooo  ccccc▒ B(▒lcccoolllllllllBBBBB(((
+(((B BBBB lllooo  oo  oooc  ccl▓▓▒▓▓(ccoollllllllllBBBBB(((
+((((B BBBB llloooo  oo  oooo   ▒▓█▓▒░oooollllllllllBBBB((((
+""".replace('B', " ").replace("(", " ").replace('l', " ").replace('o', ' ').replace('c', " "),
+4: """
+░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((    ░░░░
+░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((   ░░
+  ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓▓llB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+  ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█((((░██░      
+ (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓▓ooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░ ▓      
+ (░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB████▒▒(    
+((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓▓ol▓▓ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓░   █(((  
+    ((((BBBBllllllloBooooooolooooooollolllll BBBB((((  ▓▓█▓
+      ((  ((BBBB(▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓ BBBB((((
+     (((B  BBBlllllloool▒        ░        ▒(oooolllllBBBB(((
+ (   (  ((BB  BBllllooool░ occcl▓▓▓lcccc ░ oooolllBBBB(((((
+ ((   BB  BBBB  llloooooo ░ occl░(░(cco ░oooolllllBBB(((( 
+ ((B B  BB  Blll  looooooo ░░B(l▓▒░l(B(░░oooollllllBBBB((((
+( (BB BB  ll  lloo  ooooocco ▓ lB▓Bl ▓░occcollllllllBBBB(((
+(( BBB BBl  ll  looo  oooccccc▒▓▓█▓▓▓░ccccoollllllllBBBBB((
+(( BBBB Blll  lo  oooo  occccc(░B(B░░ccccoollllllllBBBBBB((
+((B BBBB llllo  oo  oooo  ccccc▒ B(▒lcccoolllllllllBBBBB(((
+(((B BBBB lllooo  oo  oooc  ccl▓▓▒▓▓(ccoollllllllllBBBBB(((
+((((B BBBB llloooo  oo  oooo   ▒▓█▓▒░oooollllllllllBBBB((((
+""".replace('B', " ").replace("(", " ").replace('l', " ").replace('o', ' ').replace('c', " "),
+5: """
+░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((    ░░░░
+░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((   ░░
+  ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓▓llB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+  ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█((((░██░      
+ (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓▓ooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░ ▓      
+ (░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB████▒▒(    
+((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓▓ol▓▓ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓░   █(((  
+    ((((BBBBllllllloBooooooolooooooollolllll BBBB((((  ▓▓█▓
+      ((  ((BBBB(▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓ BBBB((((
+     (((B  BBBlllllloool▒        ░        ▒(oooolllllBBBB(((
+ (   (  ((BB  BBllllooool░ occcl▓▓▓lcccc ░ oooolllBBBB(((((
+ ((   BB  BBBB  llloooooo ░ occl░(░(cco ░oooolllllBBB(((( 
+ ((B B  BB  Blll  looooooo ░░B(l▓▒░l(B(░░oooollllllBBBB((((
+( (BB BB  ll  lloo  ooooocco ▓ lB▓Bl ▓░occcollllllllBBBB(((
+(( BBB BBl  ll  looo  oooccccc▒▓▓█▓▓▓░ccccoollllllllBBBBB((
+(( BBBB Blll  lo  oooo  occccc(░B(B░░ccccoollllllllBBBBBB((
+((B BBBB llllo  oo  oooo  ccccc▒ B(▒lcccoolllllllllBBBBB(((
+(((B BBBB lllooo  oo  oooc  ccl▓▓▒▓▓(ccoollllllllllBBBBB(((
+((((B BBBB llloooo  oo  oooo   ▒▓█▓▒░oooollllllllllBBBB((((
+(((((B BBBl lllooooo  oooo (   ▒▒(  olllllllllllBBBB(((((
+((((((B BBBl l  lloooo  oooo   ▒░(  oolllllllllBBBB((((((
+ (((((BB  BB  ll llllllo  o ooB░░(  (olllllllllllBBB(((((( 
+  (((((BB   BB  ll lllllll  lll▒▒B  lllllllllllBBBBB(((((  
+   (((((BB   B  BB  B Blllllll ░▓(   llllllllBBBBB((((((   
+    ((((((B   BB  BB   BBBllll(▒▒(   llllBBBBBBB(((((((    
+     ((((((( (  BB  B   BBBBBBB▒▒B  BBBBBBBBB((((((((             
+░░       (((( ((  ((   (  BBBBB░▒   BBBBBB((((((((                ░░
+░░░░        (( (((  (   ((  (((▓▓(  (((((((((((                 ░░░░
+░░░░░░            (( (  ((((((((((((                          ░░░░░░
+░░░░░░░░░░                (                               ░░░░░░░░░░  
+""".replace('B', " ").replace("(", " ").replace('l', " ").replace('o', ' ').replace('c', " "),
+6: """
+░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((    ░░░░
+░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((   ░░
+  ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓▓llB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+  ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█((((░██░      
+ (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓▓ooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░ ▓      
+ (░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB████▒▒(    
+((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓▓ol▓▓ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓░   █(((  
+    ((((BBBBllllllloBooooooolooooooollolllll BBBB((((  ▓▓█▓
+      ((  ((BBBB(▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓ BBBB((((
+     (((B  BBBlllllloool▒        ░        ▒(oooolllllBBBB(((
+ (   (  ((BB  BBllllooool░ occcl▓▓▓lcccc ░ oooolllBBBB(((((
+ ((   BB  BBBB  llloooooo ░ occl░(░(cco ░oooolllllBBB(((( 
+ ((B B  BB  Blll  looooooo ░░B(l▓▒░l(B(░░oooollllllBBBB((((
+( (BB BB  ll  lloo  ooooocco ▓ lB▓Bl ▓░occcollllllllBBBB(((
+(( BBB BBl  ll  looo  oooccccc▒▓▓█▓▓▓░ccccoollllllllBBBBB((
+(( BBBB Blll  lo  oooo  occccc(░B(B░░ccccoollllllllBBBBBB((
+((B BBBB llllo  oo  oooo  ccccc▒ B(▒lcccoolllllllllBBBBB(((
+(((B BBBB lllooo  oo  oooc  ccl▓▓▒▓▓(ccoollllllllllBBBBB(((
+((((B BBBB llloooo  oo  oooo   ▒▓█▓▒░oooollllllllllBBBB((((
+(((((B BBBl lllooooo  oooo (   ▒▒(░▒olllllllllllBBBB(((((
+((((((B BBBl l  lloooo  oooo   ▒░(░▒oolllllllllBBBB((((((
+ (((((BB  BB  ll llllllo  o ooB░░(░▒(olllllllllllBBB(((((( 
+  (((((BB   BB  ll lllllll  lll▒▒B▒▓lllllllllllBBBBB(((((  
+   (((((BB   B  BB  B Blllllll ░▓(▓░ llllllllBBBBB((((((   
+    ((((((B   BB  BB   BBBllll(▒▒(▒░ llllBBBBBBB(((((((    
+     ((((((( (  BB  B   BBBBBBB▒▒B░▒BBBBBBBBB((((((((             
+░░       (((( ((  ((   (  BBBBB░▒ ▒░BBBBBB((((((((                ░░
+░░░░        (( (((  (   ((  (((▓▓(▒▒(((((((((((                 ░░░░
+░░░░░░            (( (  ((((((((((((                          ░░░░░░
+░░░░░░░░░░                (                               ░░░░░░░░░░ 
+""".replace('B', " ").replace("(", " ").replace('l', " ").replace('o', ' ').replace('c', " ")
+
+}
+
+
+big_man="""
+░░░░░         (((((((((((BBBBBBBBBBBBBBBBBBBBBBBBB(((((((((((((         ░░░░
+░░         (((((((((BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB(((((((((        ░░
+░     ░▓▓▓▓▓((░▒BBB ▒BBB(▓▓░BlB▓▓▓▓▓BllB▓▓▓▓▓B(▒BBB ▓B ▒BBB(▓(((((░▓       
+      ░▓(((▒▒B░▒BB(▓▒B(▒░lll▓ l▓BlllllllB▓llB█B(▓llB▓▓B░▒BBB(█(((((██░      
+     (░█▓▓▓░(B░▒B(▓▒▒l▓ lllllll▓▓▓▓▓oooB▓lll▓B(▓l(▓░▓l░▒BBB(█(B((▓░░▓      
+    ((░▓(((░▒B░▒B▓(░▒l▒▒lllllol▓BoooooooB▓lol▓B(▓B▓((▓l░▒llBB█(BB░▒  ▒▒(    
+   (((░▓▒▓▓▓▒B░▓▓(l ▒ll░▓▒▓▓ll ▓▓▓▓▓lool▒░ool▓B(▓▓Bl(▓l ▒▒▓▓▓█(B ▓    █(((  
+  (((((    BBB((lllB(llll((Booo((((((oB(loooo(ll(oool(lB(((   ▓ ((BBB(((((  
+ ((((((BBBBBllllllllllool░░Booooooooo(░(ooooooooo(░░lollllllllllBBBBBB((((((
+((((((BBBBBBl▓▓▒▒▒▒▒▒▒▒▒▓▒▒▓▒▒▒░░░░░▒▓▒▓▒░░░░▒▒▒▒▓▒▓▓▒▒▒▒▒▒▒▒▒▓░lBBBBBB(((((
+(((((BBBBBBllBllllllllll░▓▓▓looooooooB▓▒oooooooo(▓▓▓(lllllllllllllBBBBBB((((
+(((((BBBBBBBllllllloooooo░ ░ooocccccc ▓(ccccccool░░ oooooollllllBBBBBBB(((((
+(((((BBBBBBBllllllloooooo▒ ▒  occccco▓█▓▓░occcccc░(  oooooollllllBBBBB(((((((
+(((((BBBBBBBBlllllooooooo░ (▒occccc(▓   ▓occcccB░(░ oooooolllllBBBBB((((((((
+(((BBBBBBBBllllllooooooooB▒  ░(occcl▒▒░▒▒occcl  ( ▒oooooolllllllBBBBB(((((( 
+(((BBBBBBBllllllloooooooooo░▒ (BB((cB▓▓▓oo((BB(░▒ ooooolllllllllBBBBBB((((((
+(((BBBBBBBlllllloooooooooococ░▒░ ▒▓▒░░ ▒▒▓▒▓ ▓▒ occocoollllllllllBBBBBB(((((
+(((BBBBBBBlllllloooooooooccccco ▓▓▓▓ol(lo▓▓▓▓ ccccccoollllllllllllBBBBBB((((
+(((BBBBBBBllllllooooooooocccccccc▒▒Bll(ll(▓░ccccccccollllllllllllBBBBBBBB(((
+(((BBBBBBBlllllloooooooooccccccccB▓▒ (((░▒█occccccooolllllllllllllBBBBBBBB((
+(((BBBBBBBlllllloooooooooccccccccc▒ ( ( (░░ccccccoooollllllllllllBBBBBBBB(((
+(((BBBBBBBBllllloooooooooooccccccc ▒ (B(░▒BccccccooollllllllllllBBBBBBBB((((
+(((BBBBBBBBllllloooooooooooocccccc ▒░░░░░▓BccccoooolllllllllllllBBBBBBBB((((
+(((((BBBBBBBllllloooooooooooocccco▓▓▓▓▒▓▓▓▒cccoooolllllllllllllllBBBBBBB((((
+((((((BBBBBBBllllloooooooooooooooB▓▒▓▓█▓▓▒▓oooooollllllllllllllllBBBBBB(((((
+((((((((BBBBBBlllllooooooooooooooB▒( ███((▒oooolllllllllllllllBBBBBBB(((((((
+((((((((BBBBBBlllllllooooooooooooB░((▒l▓((▒loollllllllllllllllBBBBBB((((((((
+(((((((((BBBBBlllllllloooooooooooB░( ▒l▓((▒loooollllllllllllllBBBBB(((((((((
+ ((((((((BBBBBBllllllllooooooooooB▒  ▒l▒( ▒lololllllllllllllllBBBB((((((((((
+  (((((((BBBBBBBlllllllllllooooooo▒░ ▒l▒ ░ oollllllllllllllllBBBBBB(((((((( 
+   (((((((BBBBBBBlllllllllllllllllB▓▒▒l▓▒▓lllllllllllllllllBBBBBBB(((((((   
+    (((((((BBBBBBBBlllllllllllllll▓▒▒▒l▓▒▒░llllllllllllllBBBBBB(((((((((    
+     ((((((((BBBBBBBBBBllllllllll(▓░▒▓B▓░░▓llllllllllllBBBBBBB(((((((((     
+       ((((((((BBBBBBBBBBBllllllll▓░▒▓B▓░░░llllllllBBBBBBBBBB(((((((((      
+       (((((((((((BBBBBBBBBBBBBBBBB▓▒ B░▒▒BBBBBBBBBBBBBBBB((((((((((        
+░        ((((((((((((BBBBBBBBBBBBBB░▒░B▒▒ BBBBBBBBBBBB(((((((((((           
+░░░         ((((((((((((((BBBBBBBBB░▒▒B▓▒ BBBBBBBBB((((((((((((             
+░░░░░░          ((((((((((((((((BB░▒░▒(▓░▒ BB((((((((((((((                ░
+░░░░░░░             (((((((((((((((▓▓ (░▓▒((((((((((((((((              ░░░░
+░░░░░░░░                   (((((((((((((((((((((((                 ░░░░░░░░░
+░░░░░░░░░░░                      (((((((((                        ░░░░░░░░░░
+░░░░░░░░░░░░░░░░                                            ░░░░░░░░░░░░░░░░
+
+"""
+litle_man=litle_man.replace('B', " ").replace("(", " ").replace('l', " ").replace('o', ' ').replace('c', " ")
+print(litle_man)
